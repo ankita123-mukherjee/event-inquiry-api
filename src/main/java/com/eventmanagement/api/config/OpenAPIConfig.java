851093702142730c9560d6c0fb2deda524aa0cf7
ankita123-mukherjee <ankita2026@gmail.com>
2026-07-31 00:49:27 +0530

    Update project files and latest code changes

--- src/main/java/com/eventmanagement/api/config/OpenAPIConfig.java
+++ src/main/java/com/eventmanagement/api/config/OpenAPIConfig.java
@@ -7,8 +7,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -17,6 +20,7 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .servers(List.of(new Server().url("/").description("Current Environment Server")))
                 .info(new Info()
                         .title("Event Inquiry Management API")
                         .version("1.0.0")
